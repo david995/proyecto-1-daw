2f1509b060537b74dbc892a694623b128df08c88
LluisAntoniFerrer <sarke56@gmail.com>
2015-05-22 13:25:58 +0200

    proyecto

---
+++ proyecto 1 daw/src/proy/Main.java
@@ -0,0 +1,16 @@
+package proy;
+
+import java.awt.EventQueue;
+
+import Interfaces.Inicio;
+import Interfaces.InterfazRegistro;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		
+		Inicio.invoke();
+	
+	}
+
+}
